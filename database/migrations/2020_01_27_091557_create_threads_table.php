1de32a36530527f1d30ee9ec7f913a3e636d5de9
katoso <kao.kao.722@outlook.jp>
2020-01-27 18:55:12 +0900

    first push

---
+++ database/migrations/2020_01_27_091557_create_threads_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateThreadsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('threads', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('threads_id');
+            $table->string('title');
+            $table->timestamps();
+            $table->softDeletes();
+            $table->integer('cates_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('threads');
+    }
+}
---
+++ database/migrations/2020_01_27_091749_create_cates_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCatesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cates', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('cates_id');
+            $table->string('cates_name');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cates');
+    }
+}
