c6c49d6007ee3fe2497059ee6c651e4970bde99b
octkmr <octkmr@gmail.com>
2020-01-28 18:20:38 +0900

    いろいろ修正しました

--- resources/views/new_thread_create.blade.php
+++ resources/views/new_thread_create.blade.php
@@ -3,8 +3,19 @@
 @section('title', '新規スレ作成')
 @section('content')
 
-
-
+<h1>スレッドを作成する</h1>
+<form action="{{ url('/new') }}" method="post"> 
+@csrf
+  <p>スレタイを入力してください</p>
+  <p><input type="text"></p>
+  <p>カテゴリを選択してください</p>
+  <select>
+    <option value="" hidden>カテゴリを選択してください</option>
+    <!-- foreachでoptionを回す -->
+    <option value="カテゴリid">カテゴリ名</option>
+  </select>
+  <p><input type="submit" value="作成する"></p>
+</form>
 
 
 @endsection
--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -3,4 +3,23 @@
 @section('title', '検索結果')
 @section('content')
 
+@if(true)
+<h1>「」の検索結果</h1>
+<p>〇件</p>
+@else
+<h1>カテゴリ「」の結果</h1>
+<p>〇件</p>
+@endif
+
+<table>
+  <tr><td><a href="{{url('/read')}}">タイトル</a></td><td>レス数</td><td>作成日時</td><td>最終投稿日時</td></tr>
+  <!-- foreach -->
+</table>
+
+<ul>
+  <li>1</li>
+  <li>2</li>
+  <li>3</li>
+</ul>
+
 @endsection
