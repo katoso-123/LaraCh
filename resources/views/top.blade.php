8306283e131f5cd44b5a3b3584749d83309a9489
katoso <kao.kao.722@outlook.jp>
2020-01-28 17:22:54 +0900

    top.blade.phpを更新しましたよ

--- resources/views/top.blade.php
+++ resources/views/top.blade.php
@@ -3,4 +3,22 @@
 @section('title', 'Larach')
 @section('content')
 
+  <h1>スレッド検索</h1>
+  <form action="{{ url('/word') }}">
+    <p>ワードで検索！</p>
+    <p><input type="text" placeholder="検索ワードを入力してください"><input type="submit"></p>
+  </form>
+  <form action="{{ url('/category') }}">
+    <p>カテゴリで検索！</p>
+    <p>
+      <select>
+      <option value="" hidden>カテゴリを選択してください</option>
+      <!-- foreachでoptionを回す -->
+      <option value="カテゴリid">カテゴリ名</option>
+      <input type="submit">
+    </p>
+  </form>
+  <h1>新規スレッド作成</h1>
+  <a href="{{ url('/new') }}">新規スレッド作成</a>
+
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -11,8 +11,4 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/top','TopController@top');
+Route::get('/','TopController@top');
