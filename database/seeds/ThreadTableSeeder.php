84c96e43fb6f1eb17f49a9868022d7d5e7747cd9
octkmr <octkmr@gmail.com>
2020-01-30 23:06:46 +0900

    ThreadTableSeeder

---
+++ database/seeds/ThreadTableSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use App\Thread;
+
+class ThreadTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        DB::table('threads')->insert([
+            [
+                'threads_id' => 1,
+                'title'      => 'コロナウイルス最後まで感染者が出ない県どこか予想しようぜ',
+                'created_at' => new DateTime(),
+                'updated_at' => new DateTime(),
+                'deleted_at' => new DateTime(),
+                'cates_name' => '一般',
+            ],
+        ]);
+    }
+}
